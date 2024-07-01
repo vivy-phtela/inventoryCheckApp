15e5b7ac283a63e468102b6bbe4521445c118fed
vivy_phtela <tubasa.0405@icloud.com>
2024-07-02 03:04:33 +0900

    デプロイテスト

--- utils/spabase.js
+++ utils/spabase.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log(process.env.REACT_PUBLIC_SUPABASE_URL);
+console.log(process.env.REACT_PUBLIC_SUPABASE_ANON_KEY);
+
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.REACT_PUBLIC_SUPABASE_URL,
+  process.env.REACT_PUBLIC_SUPABASE_ANON_KEY
 );
