fc80b2dab1ad27b761345c49159b902c399990d6
vivy_phtela <tubasa.0405@icloud.com>
2024-10-02 18:13:36 +0900

    delete: 移行に伴うファイルの削除

--- utils/spabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// console.log(process.env.REACT_PUBLIC_SUPABASE_URL);
-// console.log(process.env.REACT_PUBLIC_SUPABASE_ANON_KEY);
-
-export const supabase = createClient(
-  process.env.REACT_PUBLIC_SUPABASE_URL,
-  process.env.REACT_PUBLIC_SUPABASE_ANON_KEY
-);
