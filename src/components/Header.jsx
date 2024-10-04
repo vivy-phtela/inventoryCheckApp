91b886bd5c0a37c49a1ea8eb23fbf0c15538697a
vivy_phtela <tubasa.0405@icloud.com>
2024-10-04 22:35:57 +0900

    fix: レイアウトを調整

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 // ヘッダーコンポーネント
 const Header = ({ dailyCheckStatus }) => (
-  <div className="d-flex align-items-center mb-4">
+  <div className="d-flex align-items-center">
     <h1 className="fw-bold">Daily棚卸</h1>
     <span className="fw-bold ms-3">{dailyCheckStatus}</span>
   </div>
